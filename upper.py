8ac055855d7b7270ab722221d009f83f9b9dd65c
Ken <2303336+KDPryor@users.noreply.github.com>
2022-09-03 21:03:31 -0500

    Update upper.py
    Added line 15 which will handle upper or lower case input when user enters Y or N.

--- upper.py
+++ upper.py
@@ -1,16 +1,18 @@
-#upper.py ver. 1.2 by Ken Pryor 09/02/2022
-#Accepts input of lower/mixed case text,#converts it to uppercase & sends output to the clipboard. Offers to output to a text file as well.
+#upper.py ver. 1.3 by Ken Pryor 09/02/2022
+#Accepts input of lower/mixed case text, converts it to uppercase & sends output
+#to the clipboard. Offers to output to a text file as well.
 
 import pyperclip as pc
 
-text = input('What do you wish to convert?\n')
+text = input('What do you wish to convert?\r\n')
 
 uppercase = text.upper()
 pc.copy(uppercase)
 
 print('Your text is now in the clipboard. Do you wish to me to place it in a text file for you?\n')
 
-answer1 = input('Y or N\n')
+answer1 = input('Y or N\r\n')
+answer1 = answer1.lower()
 
 if answer1 == 'y':
 	f=open("output.txt", "a+")
