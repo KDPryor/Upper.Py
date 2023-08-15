60e58ed5869479546f08253f98916b9c7e5aacc8
Ken <2303336+KDPryor@users.noreply.github.com>
2023-08-15 09:59:07 -0500

    version 1.1
    Added a clear button to remove contents of text window

--- upper-gui.py
+++ upper-gui.py
@@ -1,13 +1,21 @@
-# Upper GUI version 1 by Ken Pryor 09/24/2022
+# Upper.Py version 1.1
+# Added Clear button
+# by Ken Pryor
+# 10/14/2022
 
 from tkinter import *
 import pyperclip as pc
 
 root = Tk()
-root.title("Upper")
+root.title("UpperPy")
+root.geometry("500x150")
 
-e = Entry(root, width = 100)
-e.pack()
+e=Entry(root, width=150, font=("Arial 18"))
+e.pack(pady=20)
+
+def paste():
+    clipboard = root.clipboard_get() # Get the copied item from system clipboard
+    e.insert('end',clipboard) # Insert the item into the entry widget
 
 def myClick():
     t = e.get()
@@ -18,6 +26,15 @@ def myClick():
     e.insert(0, u)
     myLabel.pack()
     
-myButton = Button(root, text = 'Enter your text', command=myClick)
+def myDelete():
+    e.delete(0, END)
+    
+
+
+myButton = Button(root, text = 'Paste', height=1, width=10, command=paste)
+myButton.pack()      
+myButton = Button(root, text = 'Uppify It!', height=1, width=10, command=myClick)
 myButton.pack()
+myDeleteButton = Button(root, text = 'Clear', height=1, width=10, command=myDelete)
+myDeleteButton.pack()
 root.mainloop()
